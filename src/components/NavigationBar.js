43d8c5c9d09b61f7bb798e450ecc61676941d39b
onetimejob <one1timejob@gmail.com>
2020-09-09 15:21:03 +0300

    Adding NavigationBar

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  './Navigation.css'
+export default function NavigationBar()
+{
+    return(
+        <div>
+            <a href="onetimejob">כניסה</a>
+            <a href="onetimejob">הרשמה</a>   
+             <a href="onetimejob">עלינו</a>
+            <a href="onetimejob">עזרה</a>
+        </div>
+    )
+}
