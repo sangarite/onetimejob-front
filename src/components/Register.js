2bd706bd15eb5bd8ce018564efacd5acfccdd944
onetimejob <70694641+onetimejob@users.noreply.github.com>
2020-09-29 16:23:05 +0300

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,9 +3,15 @@ import React from 'react'
 export default function Register()
 {
     return(
-      <div className="topSpace">
+     <div className="topSpace">
         <br />
-        <p>Register</p>
+      {//maybe we should use form to send all the details together
+}
+        <p className="form">הכנס את שמך</p>
+        <input id="name" spellCheck="false" maxLength="25" ></input>
+        <p className="form">בחר סיסמה</p>
+        <input id="password" type="password" maxLength="9"></input>
+        <button id="savedetails">שמירה</button>
       </div>
     )
 }
