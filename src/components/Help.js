5b1a82510446bbc5b37ab1c1753f1e6b18176426
sangarite <70514899+onetimejob@users.noreply.github.com>
2020-10-22 22:04:12 +0300

    conecting to server

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component} from 'react'
 
-export default function Help()
+export default class Help extends Component
 {
-    return(
-      <div className="topSpace">
-        <br />
-        <p>Help</p>
-      </div>
-    )
+    constructor() {
+      super();
+      this.state = {
+        data: []
+      }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/')
+        .then(response => response.json())
+        .then(data => this.setState({data: data.recordset[0].name}))
+        .catch(error => console.log(error))
+    }
+    render() {
+      return(
+        <div className="topSpace">
+          {this.state.data}
+          </div>
+        )
+    }
 }
