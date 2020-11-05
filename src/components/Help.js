2dc46101a9923e974029b6942c4868736906abb7
sangarite <70514899+onetimejob@users.noreply.github.com>
2020-11-05 11:56:35 +0200

    help

--- src/components/Help.js
+++ src/components/Help.js
@@ -10,15 +10,22 @@ export default class Help extends Component
     }
 
     componentDidMount() {
-        fetch('http://localhost:3000/')
+        fetch('http://localhost:3000/help')
         .then(response => response.json())
-        .then(data => this.setState({data: data.recordset[0].name}))
+        .then(data => this.setState({data: data}))
         .catch(error => console.log(error))
     }
+
     render() {
       return(
         <div className="topSpace">
-          {this.state.data}
+        <br/>
+        <br/>
+          {
+            this.state.data.map((question) =>
+              <p key={question.id}>Q: {question.question} <br/>A: {question.answer}</p>
+            )
+          }
           </div>
         )
     }
