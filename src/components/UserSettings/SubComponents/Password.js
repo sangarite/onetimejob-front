acb6c3af2b253ac6dac16d0ac583011c3d3e87e0
sangarite <70514899+onetimejob@users.noreply.github.com>
2020-11-08 11:32:22 +0200

    password

--- src/components/UserSettings/SubComponents/Password.js
+++ src/components/UserSettings/SubComponents/Password.js
@@ -2,9 +2,35 @@ import React from 'react';
 
 class Password extends React.Component {
 
-	onButtonSubmit = (event) => {
-		//confirm the old Password
-		//confirm the new password
+	constructor(props) {
+		super(props);
+		this.state = {
+			newPassword: '',
+			oldPassword: '',
+			confirmPassword: ''
+		}
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onButtonClick = this.onButtonClick.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({[`${event.target.id}`]: event.target.value})
+	}
+
+	onButtonSubmit(event) {
+		fetch('http://localhost:3000/settings/password', {
+			method: 'put',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				id: this.props.user.user_id,
+				newPassword: this.state.newPassword,
+				oldPassword: this.state.oldPassword,
+				confirmPassword: this.state.confirmPassword
+			})
+		})
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(err => console.log(err))
 	}
 
 	render() {
