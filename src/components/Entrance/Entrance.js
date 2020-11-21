7f789df0a8ab0dc9d38f73fe93f88a57c6c60b8f
sangarite <70514899+onetimejob@users.noreply.github.com>
2020-11-21 18:05:42 +0200

    home design

--- src/components/Entrance/Entrance.js
+++ src/components/Entrance/Entrance.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import img from './team_work.png';
 import './Entrance.css';
-
-
+import home_background from '../../images/home_background.png';
 
 class Entrance extends Component {
   render() {
     return(
-      <div id="frame" className="topSpace">
-        <div id="image"><img src={img} alt="team_work" /></div>
-        <div id="left">
-          <h1>רךחמכיהנ גחי סמהד</h1>
-          <p>כחג כיקו כידל שסזע המסיכ החגכלחד כדיחלקיד כוקםכד חגלכךד דחלכד
-          גיכדלחכיד כדךלגםן דםחג עררי חדךמנבי דךלחםן ופןא תפווד 'וקה כחדד
-          כדךח' דבשינו גלדד קודל קפודס רואינם חםוא כחנכחן סמהצתד דיכלדח כיד
-          דיךד קיםן' דיגםכן' דיםכקן דיםקן דלחכ'ם ן נתצבץח נמץב תמילה תד'
-          </p>
-          <button><Link to="/jobs">חיפוש עבודה</Link></button>
-          <button><Link to="/publish">פרסום עבודה</Link></button>
-        </div>
+      <div id="entrance">
+        <img src={home_background} alt={home_background}/>
+        <p>"ההוצאה הגדולה ביותר היא איבוד זמן"</p>
+        <Link to="/jobs">חיפוש עבודה</Link>
+        <div id="benefits"></div>
+        <div id="about"></div>
       </div>
     );
   }
