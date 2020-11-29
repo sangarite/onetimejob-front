6a21d15e24417e1a098b862a56b81eeffcaa9adf
sangarite <70514899+onetimejob@users.noreply.github.com>
2020-11-29 12:00:15 +0200

    styling job

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import Swal from 'sweetalert2'
 import './Job.css'
+import area from '../../images/area.png'
+import city from '../../images/city.png'
+import expiry_date from '../../images/expiry_date.png'
+import salary from '../../images/salary.png'
 
 export default function Jobs(props) {
 
   const history = useHistory();
 
-  const info = props.jobs.filter((job) => job.job_id === props.match.params.id);
-
-  const [comment, setComment] = React.useState('');
+  const info = props.jobs.filter((job) => job.job_id === parseInt(props.match.params.id));
+  console.log(info);
 
   const onButtonClick = () => {
     if (Object.keys(props.user).length === 0) {
@@ -17,43 +20,67 @@ export default function Jobs(props) {
         text: 'יש להיכנס תחילה',
         icon: 'warning',
         confirmButtonText: 'העבר אותי לכניסה'
-      })
-      history.push('/signin');
+      }).then((res) => { if(res.value) history.push('../signin')})
     } else {
-      fetch('http://localhost:3000/job/apply', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          job_id: info[0].job_i,
-          user: props.user,
-          publisher_id: info[0].user_id,
-          comment: comment
-        })
+      Swal.fire({
+        title: 'שכנע את המעסיק שהעבודה מתאימה לך (עד 50 תווים)',
+        input: 'text',
+        confirmButtonText: 'שלח',
+        showLoaderOnConfirm: true,
+        preConfirm: (comment) => {
+          fetch('http://localhost:3000/job/apply', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              job_id: info[0].job_i,
+              user: props.user,
+              publisher_id: info[0].user_id,
+              comment: comment
+            })
+          })
+          .then(response => response.text())
+          .then(data => {
+            data === 'success' ?
+            Swal.fire({
+              text: 'בקשתך התקבלה. פרטייך נשלחו בהצלחה',
+              icon: 'success',
+              showConfirmButton: false,
+              timer: 3000
+            }) :
+            Swal.fire({
+              text: 'ישנה בעיה. נסה שוב או דווח על הבעיה  בעזרה',
+              icon: 'error',
+              showConfirmButton: false,
+              timer: 3000
+            })
+          })
+        }
       })
-      .then(response => response.text())
-      .then( data => data === 'success' ? Swal.fire({
-            text: 'בקשתך התקבלה. המעסיק יצור עמך קשר במידה ויהי מעוניין',
-            icon: 'success',
-            showConfirmButton: false,
-            timer: 2000
-          }) : Swal.fire({
-            text: 'ישנה בעיה. נסה שוב או דווח על הבעיה  בעזרה',
-            icon: 'error',
-            showConfirmButton: false,
-            timer: 2000
-          }))
     }
   }
 
-  const onTextChange = (event) => {
-    setComment(event.target.value);
-  }
-
   return(
-    <div className="job">
+    <div id="job">
+      <div className="icons">
+        <div className="icon">
+          <img src={area} alt="area" />
+          <p>{info[0].area}</p>
+        </div>
+        <div className="icon">
+          <img src={city} alt="city" />
+          <p>{info[0].city}</p>
+        </div>
+        <div className="icon">
+          <img src={expiry_date} alt="expiry_date" />
+          <p>{info[0].expiry_date}</p>
+        </div>
+        <div className="icon">
+          <img src={salary} alt="salary" />
+          <p>{info[0].salary}</p>
+        </div>
+      </div>
       <h2>{info[0].title}</h2>
       <p>{info[0].details}</p>
-      <textarea onChange={onTextChange}></textarea>
       <button onClick={onButtonClick}>מעוניין</button>
     </div>
   );
