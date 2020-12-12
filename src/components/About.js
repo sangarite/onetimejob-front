b93c363be705453530f96fbd3240e09decd0b1fc
onetimejob <one1timejob@gmail.com>
2020-12-12 22:30:22 +0200

    Design About us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import l from '../images/logo.png'
+import logo from './../images/logo.png'
+import { Link } from 'react-router-dom'
 
+import './About.css'
 export default function About()
 {
     return(
       <div className="topSpace">
-      <p>?השתמשתם פעם בכוס חד פעמית, לשתייה קרה או חמה
+      <p >?השתמשתם פעם בכוס חד פעמית, לשתייה קרה או חמה
         <br />
         ?קבלתם כוס חדשה, שתיתם, נהניתם ו.. גמרתם איתה לתמיד
         <br></br>            
-        <h3>,גם עבודה יכולה להיות חד פעמית  <br />
+        <h3 className="text">,גם עבודה יכולה להיות חד פעמית  <br />
       !!חדשה, מעניינת, מתגמלת ו.. קצרה</h3>
     
-     <image src={l} id="logo" alt="wow">זו התמונה</image><br />
+     <image src={logo} id="logo" alt="wow">זו התמונה</image><br />
 .הוקם עבור כל המעסיקים המחפשים עובדים לתקופות קצרות  <br />
 ?כמו מה למשל, אתם שואלים
 <br />
@@ -20,25 +22,23 @@ export default function About()
 :או כל עבודה אחרת הקשורה לתקופות מסוימות בשנה
 <br />
 אריזת חנוכיות בערב חנוכה, מכירת תחפושות לקראת פורים ועוד
-<br /> <image src={l} id="logo" alt="wow">זו התמונה</image><br />
+<br /> <image src={logo} id="logo" alt="wow">זו התמונה</image><br />
 .הוקם עבור כל העובדים המחפשים תעסוקה לטווח קצר
 <br />
 ..עובדים רבים מחפשים תעסוקה קטנה עד ש
 <br />עד שיעבור גל הקורונה והם יחזרו לעבודתם הקבועה<br />
 עד שימצאו עבודה במקצוע אותו רכשו<br />
 'עד שיבריאו, עד שיעברו דירה, עד שיתחתנו וכדו<br /><br />
-<h4>!מעסיק יקר! עובד נמרץ
-</h4>
+<h3 className="text">!מעסיק יקר! עובד נמרץ
+</h3>
 ,אם אתה אחד כזה שעונה על התאור הנ"ל
 <br />
 .זו ההזדמנות שלך להרשם באתר שלנו <br />
 .לחיצה אחת והכסף בדרך אליך<br />
-<br />להרשמה לחצו 
-<br />לצפייה בעבודות לחצו<br />
-<h2>!job זה one time</h2>
+<br /> <Link to="/register" id="link">להרשמה לחצו כאן</Link>  
+<br /><Link to='/jobs'>לצפייה בעבודות לחצו כאן</Link><br />
+<h2 className="text">!job זה one time</h2>
       
-      </p>
-      <p>
       </p>
     </div>
     )
